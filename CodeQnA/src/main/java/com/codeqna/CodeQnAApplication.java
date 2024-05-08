f24020f66e049a77cbe39110ca3518e050878923
ramperrit <153475173+ramperrit@users.noreply.github.com>
2024-05-08 17:51:13 +0900

    헤더 푸터 로그인 회원가입 페이지

---
+++ CodeQnA/src/main/java/com/codeqna/CodeQnAApplication.java
@@ -0,0 +1,13 @@
+package com.codeqna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeQnAApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeQnAApplication.class, args);
+    }
+
+}
---
+++ CodeQnA/src/test/java/com/codeqna/CodeQnAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeqna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeQnAApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
